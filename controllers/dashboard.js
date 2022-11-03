5c03f37cc4bf24f3b58fa00f406cffc4ea43a7cf
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2022-11-03 19:34:05 -0400

    added helpers and authorization

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,1 +1,5 @@
 const router = require('express').Router();
+
+// Todo: route for all posts - homepage
+// todo: route for single post
+// todo: route for logged in users posts
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const path = require('path')
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -10,9 +11,16 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
 const sess = {
     secret: process.env.DB_SECRET,
-    cookie: {},
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -21,8 +29,7 @@ const sess = {
 };
 
 app.use(session(sess));
-const hbs = exphbs.create({ helpers });
-
+// Passes which template is being used to Express
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
